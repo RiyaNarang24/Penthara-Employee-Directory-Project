701e5023e9519452bf9470dae803cdfe4a76b85f
RiyaNarang24 <itsriya391@gmail.com>
2026-09-17 20:32:21 +0530

    configure production CORS

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const app=express();
 const PORT=process.env.PORT||5000;
 
 //Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://penthara-employee-directory-project.vercel.app",
+  })
+);
 app.use(express.json());
 app.use("/api/employees", employeeRoutes);
 app.get("/",(req,res)=>{
