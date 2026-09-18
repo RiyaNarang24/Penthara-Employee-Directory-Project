63685e0c42165a57b0cd365d846d32b45114bb40
RiyaNarang24 <itsriya391@gmail.com>
2026-09-18 23:57:21 +0530

    Add authentication and improve backend startup

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors=require("cors");
 const dotenv=require("dotenv");
 const connectDB=require("./config/db");
 dotenv.config();
-connectDB();
+
 const app=express();
 const PORT=process.env.PORT||5000;
 
@@ -24,6 +24,8 @@ app.use("/api/auth", authRoutes);
 app.get("/",(req,res)=>{
     res.json({message:"Employee Directory API is running"});
 });
-app.listen(PORT,()=>{
+connectDB().then(() => {
+  app.listen(PORT, "0.0.0.0", () => {
     console.log(`Server running on http://localhost:${PORT}`);
+  });
 });
